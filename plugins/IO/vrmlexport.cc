52e87e0c0278a0e5f00412d09bd42215f8eed49d
ob821 <ob821@cdb957d5-0be7-0310-916f-93257de0ea17>
2004-12-06 10:38:56 +0000

    bugfix

--- plugins/IO/vrmlexport.cc
+++ plugins/IO/vrmlexport.cc
@@ -18,6 +18,8 @@ public:
   int exportData (const std::string& filename,
 		  const std::vector<Face>& faces)
   {
+    std::vector<Face>::const_iterator i;
+    int k = 0;
     FILE *file = fopen (filename.c_str (), "w");
     if (!file)
       return -2;
@@ -28,10 +30,23 @@ public:
     fprintf (file, 
 	     "      coord DEF coord_noname Coordinate {\n        point [\n");
     // COORDS
+    for (i=faces.begin ();i!= faces.end (); ++i)
+      {
+	fprintf(file, "       %f %f %f,\n", (*i)[0].getCoords ()[0], 
+		(*i)[0].getCoords ()[1], (*i)[0].getCoords ()[2]);
+	fprintf(file, "       %f %f %f,\n", (*i)[1].getCoords ()[0], 
+		(*i)[1].getCoords ()[1], (*i)[1].getCoords ()[2]);
+	fprintf(file, "       %f %f %f,\n", (*i)[2].getCoords ()[0], 
+		(*i)[2].getCoords ()[1], (*i)[2].getCoords ()[2]);
+	k+=3;
+      }
     fprintf (file, "        ]\n");
     fprintf (file, "      }\n");
     fprintf (file, "      coordIndex [\n");
     // INDEX
+    for (int j =0; j<k; j+=3)
+      fprintf(file, "     %d, %d, %d, -1,\n", j, j+1, j+2);
+    
     fprintf (file, "      ]\n    }\n  ]\n}\n");
     fclose (file);
     return 0;
